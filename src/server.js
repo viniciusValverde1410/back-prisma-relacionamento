26f7aec6a8bf919fcb3dc8d918cc87cdd7a57e62
Felipe Santos <98625123+FelipeSantos92Dev@users.noreply.github.com>
2025-04-17 07:58:11 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors"; // Importa o middleware CORS
+
+import animesRouter from "./routes/animeRoutes.js";
+
+config(); // Carrega variáveis de ambiente do arquivo .env
+const port = process.env.PORT || 4001; // Define a porta do servidor
+
+// Inicializa o Express
+const app = express();
+app.use(cors()); // Habilita CORS para todas as rotas
+
+app.use(express.json()); // Parse de JSON
+
+app.use("/animes", animesRouter); // Usar as rotas de animes
+
+// Rota base para verificar se o servidor está rodando
+app.get("/", (req, res) => {
+  res.json({ message: "API de Coleção de Animes funcionando!" });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
